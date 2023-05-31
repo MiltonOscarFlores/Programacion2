0b553ca506ea8d6f2ff1c7bae499c93c99780433
MiltonOscarFlores <miltonoscarflores@gmail.com>
2023-05-31 12:05:29 -0300

    listo el 3

---
+++ Practice3/exercise3.py
@@ -0,0 +1,81 @@
+"""Properties"""
+
+class Article:
+    iva = 0.21
+
+    def __init__(self, nombre: str, costo: float, descuento: float = 0.0):
+        self.nombre = nombre
+        self.costo = costo
+        self.descuento = descuento
+
+    @property
+    def precio(self) -> float:
+        precioConIva = self.costo * (1 + self.iva)
+        precioConDescuento = precioConIva * (1 - self.descuento)
+        return round(precioConDescuento, 2)
+
+    @precio.setter
+    def precio(self, value):
+        raise AttributeError("No se puede modificar el precio")
+
+    @classmethod
+    def actualizar_iva(cls, nuevoIva: float):
+        cls.iva = nuevoIva
+
+
+# NO MODIFICAR - INICIO
+# Test parámetro obligatorio
+try:
+    article = Article()
+    assert False, "No se puede instanciar sin nombre ni costo"
+except TypeError:
+    assert True
+
+try:
+    article = Article("Auto")
+    assert False, "No se puede instanciar sin costo"
+except TypeError:
+    assert True
+
+try:
+    article = Article(nombre="Auto", costo=1)
+    assert True
+except TypeError:
+    assert False, "El descuento es opcional"
+
+try:
+    article = Article(nombre="Auto", costo=1)
+    article.precio = 2
+    assert False, "No se puede modificar el precio"
+except AttributeError:
+    assert True
+
+
+# Test básico
+article = Article("Auto", 1)
+assert article.nombre == "Auto"
+assert article.precio == 1.21
+
+
+article = Article("Auto", 1, 0.21)
+assert article.nombre == "Auto"
+assert article.precio == 0.96
+
+
+# Test palabra clave
+article = Article(costo=1, nombre="Auto")
+assert article.nombre == "Auto"
+assert article.precio == 1.21
+
+article = Article(costo=1, nombre="Auto", descuento=0.21)
+assert article.nombre == "Auto"
+assert article.precio == 0.96
+
+
+# Test de método de clase
+Article.actualizar_iva(0.25)
+
+article = Article(costo=1, nombre="Auto")
+assert article.nombre == "Auto"
+assert article.precio == 1.25
+# NO MODIFICAR - FIN
